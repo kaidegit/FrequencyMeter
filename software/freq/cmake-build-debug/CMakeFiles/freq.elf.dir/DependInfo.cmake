7ff0ea7778ef8f3e7039949c8c5cc408f57981ed
kai <2857693944@qq.com>
2021-02-13 13:59:01 +0800

    first upload

---
+++ software/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/DependInfo.cmake
@@ -0,0 +1,75 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/kai/Desktop/mySync/mySTProject/freq/startup/startup_stm32g070xx.s" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/startup/startup_stm32g070xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32G070xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32G0xx_HAL_Driver/Inc"
+  "../Drivers/STM32G0xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32G0xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Core/Src/gpio.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Core/Src/gpio.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Core/Src/main.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Core/Src/main.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Core/Src/stm32g0xx_hal_msp.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Core/Src/stm32g0xx_hal_msp.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Core/Src/stm32g0xx_it.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Core/Src/stm32g0xx_it.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Core/Src/syscalls.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Core/Src/syscalls.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Core/Src/system_stm32g0xx.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Core/Src/system_stm32g0xx.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Core/Src/usart.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Core/Src/usart.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_cortex.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_cortex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma_ex.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma_ex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_exti.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_exti.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash_ex.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash_ex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_gpio.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_gpio.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr_ex.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr_ex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc_ex.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc_ex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim_ex.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim_ex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart_ex.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart_ex.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_dma.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_dma.c.obj"
+  "/Users/kai/Desktop/mySync/mySTProject/freq/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_rcc.c" "/Users/kai/Desktop/mySync/mySTProject/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_rcc.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32G070xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32G0xx_HAL_Driver/Inc"
+  "../Drivers/STM32G0xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32G0xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ software/freq/cmake-build-debug/CMakeFiles/freq.elf.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/freq.elf.dir/Core/Src/gpio.c.obj"
+  "CMakeFiles/freq.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/freq.elf.dir/Core/Src/stm32g0xx_hal_msp.c.obj"
+  "CMakeFiles/freq.elf.dir/Core/Src/stm32g0xx_it.c.obj"
+  "CMakeFiles/freq.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/freq.elf.dir/Core/Src/system_stm32g0xx.c.obj"
+  "CMakeFiles/freq.elf.dir/Core/Src/usart.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_cortex.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma_ex.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_exti.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash_ex.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_gpio.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim_ex.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart_ex.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_dma.c.obj"
+  "CMakeFiles/freq.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_rcc.c.obj"
+  "CMakeFiles/freq.elf.dir/startup/startup_stm32g070xx.s.obj"
+  "freq.elf"
+  "freq.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/freq.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
